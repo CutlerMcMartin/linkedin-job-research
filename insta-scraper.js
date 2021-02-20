78a6f634276df41e980a7c96ea48dfaf71e21227
CutlerMcMartin <cutler.mcmartin@gmail.com>
2021-02-20 00:41:33 -0600

    Changing this to a firefox extension

---
+++ insta-scraper.js
@@ -0,0 +1,9 @@
+var addButton = document.createElement('button')
+addButton.innerHTML = "doink"
+var section = document.getElementById("react-root")
+
+console.log(section)
+
+section.append(addButton)
+
+document.body.style.border = "5px solid red";
